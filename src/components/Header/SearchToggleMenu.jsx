dd9fa3a8b25491915d624d7114c089aefbd83284
smartdev0530 <brianchoi125@gmail.com>
2022-09-09 02:52:36 +0900

    fix search button.

--- src/components/Header/SearchToggleMenu.jsx
+++ src/components/Header/SearchToggleMenu.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { StrokeButton } from '../Buttons';
+import { ClickAwayListener } from '@mui/material';
 
 export default function SearchToggleMenu() {
 
@@ -13,20 +14,29 @@ export default function SearchToggleMenu() {
         marginLeft: '20px'
     }
 
+    const handleClickAway = () => {
+        setShowSearchMenu(false);
+    }
+    React.useEffect(() => {
+        console.log(showSearchMenu)
+    }, [showSearchMenu])
     return (
-        <div className='h-10 w-10 mr-4 flex flex-col items-end'>
-            <button className='w-10 h-10 text-light-blue border-light-blue border-2 rounded-md hover:bg-light-blue hover:text-white focus:bg-light-blue focus:text-white' onClick={() => setShowSearchMenu(!showSearchMenu)}>
-                <div className='h-10 flex items-center justify-center'>
-                    <FontAwesomeIcon icon={faSearch} className='m-auto' />
-                </div>
-            </button>
-            {
-                showSearchMenu && <div className='flex p-4 bg-light-blue rounded-sm z-40' id="searchBar">
-                    <input placeholder='Enter search term' className='px-2 py-3 text-lg placeholder:text-black-75 focus:outline-none'></input>
-                    <StrokeButton content='search' disabled={false} styles={searchButtonStyle}></StrokeButton>
-                </div>
-            }
-        </div>
+        <ClickAwayListener onClickAway={() => handleClickAway()}>
+
+            <div className='h-10 w-10 mr-4 flex flex-col items-end'>
+                <button className={`w-10 h-10  border-light-blue border-2 rounded-md hover:bg-light-blue hover:text-white ${showSearchMenu ? 'bg-light-blue text-white' : 'text-light-blue'}`} onClick={() => setShowSearchMenu(!showSearchMenu)}>
+                    <div className='h-10 flex items-center justify-center'>
+                        <FontAwesomeIcon icon={faSearch} className='m-auto' />
+                    </div>
+                </button>
+                {
+                    showSearchMenu && <div className='flex p-4 bg-light-blue rounded-sm z-40' id="searchBar">
+                        <input placeholder='Enter search term' className='px-2 py-3 text-lg placeholder:text-black-75 focus:outline-none'></input>
+                        <StrokeButton content='search' disabled={false} styles={searchButtonStyle}></StrokeButton>
+                    </div>
+                }
+            </div>
+        </ClickAwayListener>
 
     )
 }
